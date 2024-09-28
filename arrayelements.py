75d8bc119ba9f80ce6ed38eb2a928c45d2f32828
sudarshan <kosudarshan15@gmail.com>
2024-09-28 21:46:25 +0530

    l

---
+++ arrayelements.py
@@ -0,0 +1,11 @@
+array=[]
+
+n=int(input("Enter the range of an array: "))
+
+for i in range(n):
+
+	value=int(input("Enter the elements into an array: "))
+
+	array.append(value)
+
+print("The array elements are: ",array)
